e98d8b839914756467d2f8150159e2fdb41dc328
Vimal Ram K <vimalramk99@gmail.com>
2025-03-21 23:23:29 +0530

    cart badge style

--- src/UI/navigationbar.tsx
+++ src/UI/navigationbar.tsx
@@ -4,8 +4,13 @@ import person from '../assets/person.png';
 import searchicon from '../assets/search-icon.png';
 import cart from '../assets/cart.png';
 import { ChangeEvent } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "@reduxjs/toolkit/query";
+import { Rootstate } from "../redux/store";
 
 export const NavigationBar = (props : {onCallback : (text : string) => void}) =>{
+
+    const products = useSelector((state : Rootstate) => state.cart.addedItemsId)
 
     return(
         <div className="navigationbar">
@@ -26,8 +31,11 @@ export const NavigationBar = (props : {onCallback : (text : string) => void}) =>
                 
                 <img src={person} className="person" alt="" height={30} width={40} />
                 <Link to={"/cart"}>
-                <img src={cart} className="cart" alt="" height={30} width={30} /></Link>
-
+                <div className=" cart-badge">
+                <img src={cart} className="cart" alt="" height={30} width={30} />
+                <span className="badge">{products.length}</span>
+                </div>
+                </Link>
             </section>
         </div>
     )
