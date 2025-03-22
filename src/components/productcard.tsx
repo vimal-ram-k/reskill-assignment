51cebad852793102fdd6a917fe0b7359f4d41253
Vimal Ram K <vimalramk99@gmail.com>
2025-03-22 18:26:08 +0530

    product card padding issue fixed

--- src/components/productcard.tsx
+++ src/components/productcard.tsx
@@ -1,27 +1,41 @@
-import { useEffect, useState } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { ProductsData } from "../types/productTypes";
+import { AppDispatch } from "../redux/store";
+import { useDispatch } from "react-redux";
+import { addItemToCard } from "../redux/cartSlice/cartSlice";
 
-
-type ProductsData = {
-    id: number,
-    title: string;
-    image: string,
-    price: number
-}
 export const ProductCard = (props: { products: ProductsData[] , totalPrice : boolean  }) => {
 
  
+    const dispatch : AppDispatch = useDispatch();
 
+    function addItemtoCart (product : ProductsData){
+        dispatch(addItemToCard(product))
+    }
     return (
 
         props.products.map((item, index) =>
-            <Link to={`/${item.id}`} key={index} className="product-card">
-            <li className=" product-item">
+            <div className="product-card">
+
+            <Link to={`/${item.id}`} key={index} className="">
+            <li className="">
                 <img src={item.image} alt="" className="product-image" />
                 <h1 className="product-title">{item.title}</h1>
+                <button className="cart-rate-btn" disabled>{item.rating.rate}</button>
                 <h1>From ${item.price}</h1>
+            
             </li>
             </Link>
+        
+        <div className="product-card-footer"> 
+        <button className="cart-add-btn" onClick={() => addItemtoCart(item)}>Add to Cart</button>
+            {
+                item.id % 3 === 0 || item.id % 2 === 0 &&
+                <h1 className="cart-alert"> Hurry, Only {item.id % 2} left</h1>
+               }
+        </div>
+            </div>
+                
 
         ))
 
