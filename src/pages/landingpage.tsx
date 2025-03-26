97159d847c9f36c7a6e55973eb6588b1b0a73d9b
Vimal Ram K <vimalramk99@gmail.com>
2025-03-26 17:33:38 +0530

    landing page issue fixed

--- src/pages/landingpage.tsx
+++ src/pages/landingpage.tsx
@@ -1,85 +1,79 @@
-import { NavigationBar } from "../UI/navigationbar"
+import { NavigationBar } from "../UI/navigationbar";
 import { Spinner } from "../components/spinner";
-import {  Outlet, useLocation } from "react-router-dom"
+import { Outlet, useLocation } from "react-router-dom";
 import { lazy, Suspense, useEffect, useState } from "react";
 import { ProductsData } from "../types/productTypes";
 import axios from "axios";
 import { useDispatch } from "react-redux";
 import { addProducts } from "../redux/productsSlice/productSlice";
 import { AppDispatch } from "../redux/store";
-import spinnergif from '../assets/icons8-spinner.gif'
+import spinnergif from "../assets/icons8-spinner.gif";
 import { OfferBanner } from "../components/offerbanner";
 
-const ProductList = lazy(() => import('../components/productlist'))
+const ProductList = lazy(() => import("../components/productlist"));
 export const LandingPage = () => {
+  const [searchspiner, setSearchSpinner] = useState(true);
+
+  const [products, setProducts] = useState<ProductsData[]>([]);
+  const [searchKey, setSearchKey] = useState("");
+  const dispatch: AppDispatch = useDispatch();
+
+  useEffect(() => {
+    setTimeout(() => {
+      setSearchSpinner(false);
+    }, 2000);
+    setSearchSpinner(true);
+  }, [searchKey]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const res = await axios.get("https://fakestoreapi.com/products");
+      const data = await res.data;
+      console.log(data);
+      setProducts(data);
+      dispatch(addProducts(data));
+    };
+
+    fetchProducts();
+  }, []);
+
+  function addSearchKey(text: string) {
+    setSearchKey(text);
+  }
+
+  const location = useLocation();
+
+  const [showProductList, setShowProductList] = useState(true);
+  useEffect(() => {
+    setShowProductList(location.pathname === "/");
+    setSearchKey("");
+  },[location.pathname])
 
 
-    const [searchspiner, setSearchSpinner] = useState(true)
-
- 
-    const [products, setProducts] = useState<ProductsData[]>([]);
-    const [searchKey, setSearchKey] = useState("");
-    const dispatch : AppDispatch = useDispatch();
-
-
-    useEffect(() =>{
-        setTimeout(() =>{
-            setSearchSpinner(false)
-        } ,2000)
-        setSearchSpinner(true)
-    }, [searchKey])
-
-
-    useEffect(() =>{
-        const fetchProducts = async() =>{
-            const res = await axios.get("https://fakestoreapi.com/products")
-            const data =await res.data;
-            console.log(data)
-            setProducts(data);
-            dispatch(addProducts(data))
-        }
-
-        fetchProducts();
-    },[])
-
-
-
-    function addSearchKey (text : string){
-setSearchKey(text)
+  useEffect(() =>{
+    if(location.pathname === "/" && searchKey.length === 0){
+      window.scrollTo(0,100)
     }
+  })
 
-    const location = useLocation();
 
-    const [showProductList , setShowProductList] = useState(true);
-    useEffect(() => {
-      
-        setShowProductList(location.pathname === "/");
-        setSearchKey("")
-    });
-
-    useEffect(() =>{
-       if(location.pathname === "/"){
-        window.scrollTo(0,50)
-       }
-    })
-  
-
-    return(
-        <>
-        <NavigationBar onCallback={addSearchKey} />
-        {
-            showProductList && 
-            <Suspense fallback={<Spinner />}>
-               {searchKey.length === 0 &&  <OfferBanner />}
-               {searchKey.length !== 0 && 
-               <div>
-                <h1 className="search-result-header">Search Results {searchspiner && <img src={spinnergif} alt="" />}</h1>
-                </div>}
-        <ProductList products={products} searchKey ={searchKey} />
-            </Suspense>
-
-        }
-        <Outlet />
-        </>
-    )
-}
+  return (
+    <>
+      <NavigationBar onCallback={addSearchKey} />
+      {showProductList && (
+        <Suspense fallback={<Spinner />}>
+          {searchKey.length === 0 && <OfferBanner />}
+          {searchKey.length !== 0 && (
+            <div>
+              <h1 className="search-result-header">
+                Search Results {searchspiner && <img src={spinnergif} alt="" />}
+              </h1>
+            </div>
+          )}
+          <ProductList products={products} searchKey={searchKey} />
+        </Suspense>
+      )}
+      <Outlet />
+    </>
+  );
+};
