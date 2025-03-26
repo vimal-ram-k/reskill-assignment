3eedfacf3c7f50fd128c0b0fa60e81e4051152a5
Vimal Ram K <vimalramk99@gmail.com>
2025-03-26 15:06:17 +0530

    banner animation

--- src/components/offerbanner.tsx
+++ src/components/offerbanner.tsx
@@ -1,11 +1,29 @@
+import { useEffect } from 'react'
 import offerbanner from '../assets/5700556_21235.jpg'
 
 export const OfferBanner = () =>{
 
+    useEffect(() => {
+        const reduceBannerSize = () => {
+            const bannerImage = document.getElementById("banner-img");
+            if (bannerImage) {
+                const scrollPosition = window.scrollY;
+                const scaleValue = Math.max(0.5, 1 - scrollPosition / 1000); 
+                bannerImage.style.transform = `scale(${scaleValue})`;
+            }
+        };
+
+        window.addEventListener('scroll', reduceBannerSize);
+
+        return () => {
+            window.removeEventListener('scroll', reduceBannerSize);
+        };
+    }, []);
+
     return(
         <div className='banner'>
 
-            <img src={offerbanner} className='offer-banner' alt="" />
+            <img src={offerbanner} className='offer-banner' id="banner-img" alt="" />
         </div>
     )
 }
