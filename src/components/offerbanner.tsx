98e4a34d18399be66be985b16ef29e841607a2ca
Vimal Ram K <vimalramk99@gmail.com>
2025-03-26 14:00:59 +0530

    offerbanner issue fixed

--- src/components/offerbanner.tsx
+++ src/components/offerbanner.tsx
@@ -3,6 +3,9 @@ import offerbanner from '../assets/32991694_sl_051121_42790_108.jpg'
 export const OfferBanner = () =>{
 
     return(
+        <div className='banner'>
+
             <img src={offerbanner} className='offer-banner' alt="" />
+        </div>
     )
 }
